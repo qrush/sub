8946d173c35c64d344de6cd5cf65e34ea7f6cf4f
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-10-12 12:24:57 +0200

    prepare.sh: turn two calls of sed into one

--- prepare.sh
+++ prepare.sh
@@ -17,7 +17,7 @@ if [ "$NAME" != "sub" ]; then
   mv share/sub share/$SUBNAME
 
   for file in **/sub*; do
-    sed "s/sub/$SUBNAME/g" $file | sed "s/SUB_ROOT/$ENVNAME/g" > $(echo $file | sed "s/sub/$SUBNAME/")
+    sed "s/sub/$SUBNAME/g;s/SUB_ROOT/$ENVNAME/g" "$file" > $(echo $file | sed "s/sub/$SUBNAME/")
     rm $file
   done
 
