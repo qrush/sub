2bf26a95d313558d966949b3bebe74d0acd24f3e
Jeffrey Roberts <Jeffrey.L.Roberts@gmail.com>
2013-04-24 20:27:32 -0400

    small typo in bash completions

--- completions/sub.bash
+++ completions/sub.bash
@@ -5,7 +5,7 @@ _sub() {
   if [ "$COMP_CWORD" -eq 1 ]; then
     COMPREPLY=( $(compgen -W "$(sub commands)" -- "$word") )
   elif [ "$COMP_CWORD" -gt 1 ]; then
-  	$commands="$(sub commands ${COMP_WORDS[@]})"
+  	commands="$(sub commands ${COMP_WORDS[@]})"
   	if [ "$commands" ]; then
     	COMPREPLY=( $(compgen -W "$commands" -- "$word") )
     else
